75a4180fc85d0238bc1a0ebf2ac90274cc0105e9
WiitterSimithYU <378961614@qq.com>
2018-09-17 09:36:24 +0800

    commit

---
+++ TestVCPicker2/KKViewController.h
@@ -0,0 +1,13 @@
+//
+//  KKViewController.h
+//  TestVCPicker2
+//
+//  Created by miaomiaokeji on 2018/9/17.
+//  Copyright © 2018年 Yundi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KKViewController : UIViewController
+
+@end
---
+++ TestVCPicker2/KKViewController.m
@@ -0,0 +1,37 @@
+//
+//  KKViewController.m
+//  TestVCPicker2
+//
+//  Created by miaomiaokeji on 2018/9/17.
+//  Copyright © 2018年 Yundi. All rights reserved.
+//
+
+#import "KKViewController.h"
+
+@interface KKViewController ()
+
+@end
+
+@implementation KKViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
